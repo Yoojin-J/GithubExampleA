408b145def7b233f3ae33278c72392798599218c
JYoojin <syoojin03@gmail.com>
2024-01-17 12:51:56 +0900

    Button 변수 추가

--- app/src/main/java/com/example/githubexamplea/MainActivity.kt
+++ app/src/main/java/com/example/githubexamplea/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.githubexamplea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Button 클릭이벤트 작성
     }
 }
